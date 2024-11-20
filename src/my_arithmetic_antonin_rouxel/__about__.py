575476d97ee48a0a0df95ce24cbca7b6b2c6b95f
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2024-11-20 14:37:43 +0100

    chore: init project

---
+++ src/my_arithmetic_antonin_rouxel/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Antonin Rouxel <antonin.rouxel44@outlook.fr>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Antonin Rouxel <antonin.rouxel44@outlook.fr>
+#
+# SPDX-License-Identifier: MIT
