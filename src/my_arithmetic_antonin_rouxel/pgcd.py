ac9e1033a83a9b64ecd5c0258fdc0df7e21dd9f2
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2024-11-30 14:48:08 +0100

    chore: ajouter des annotations de type à la fonction pgcd

--- src/my_arithmetic_antonin_rouxel/pgcd.py
+++ src/my_arithmetic_antonin_rouxel/pgcd.py
@@ -1,9 +1,9 @@
-def pgcd(a, b):
+def pgcd(a: int, b: int) -> int:
     """
     Calcule le plus grand commun diviseur (PGCD) de deux nombres entiers.
 
     Cette fonction utilise l'algorithme d'Euclide pour trouver le PGCD de deux nombres entiers a et b.
-
+  
     :param a: Premier nombre entier.
     :type a: int
     :param b: Deuxième nombre entier.
